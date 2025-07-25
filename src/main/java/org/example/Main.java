88d26058127a5b69b5691ca1bb4254c1af8f9cf5
kiddo_dong <dh655933@gmail.com>
2025-07-26 02:22:31 +0900

    Spring bean

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,12 @@ import java.lang.reflect.AnnotatedArrayType;
 public class Main {
     public static void main(String[] args) {
 
+        // spring 에서 bean 추가를 가장 먼저 학습하는 이유
+        // 스프링이 bean 일부인 객체만 관리가 가능.
+
         // 스프링 컨텍스트의 인스턴스를 생성한다.
             var context = new AnnotationConfigApplicationContext();
             Parrot p = new Parrot();
+
         }
     }
